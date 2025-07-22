20530325475a8ac59583d3138d40cac5da830219
joseFranciscoHS <fco.hzsep@hotmail.com>
2025-07-22 09:15:29 -0600

    drcnet reconstruct dwi

--- DWMRI/src/drcnet/reconstruction.py
+++ DWMRI/src/drcnet/reconstruction.py
@@ -1,13 +1,17 @@
+import logging
+
 import torch
 from tqdm import tqdm
 
 
 def reconstruct_dwis(model, data_loader, device):
+    logging.info(f"Starting DWI reconstruction on device: {device}")
     model.to(device)
     model.eval()
     with torch.inference_mode():
-        for x in tqdm(data_loader, desc="Reconstructing"):
+        for x,_ in tqdm(data_loader, desc="Reconstructing"):
             x = x.to(device)
             reconstructed = model(x)
             reconstructed = reconstructed.squeeze().detach().cpu().numpy()
+    logging.info("DWI reconstruction completed.")
     return reconstructed[..., None]
